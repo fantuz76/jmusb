0c5987327185fea4961e889eae2bb902b64bf28e
fantuz76@gmail.com <fantuz76@gmail.com@ba4d8bbf-c410-466b-920d-f5df8fa453a8>
2010-12-03 16:18:04 +0000

    Primi inizi

--- trunk/ChiUSB_Sources/protocol.h
+++ trunk/ChiUSB_Sources/protocol.h
@@ -2,7 +2,27 @@
 #define __PROTOCOLH
 
 
-#define MAX_LEN_CMD   40
+// Lunghezza Massima in byte dati su USB (compresi checksum, length etc..)
+#define MAX_LEN_CMD       40
+#define MAX_LEN_PAYLOAD   MAX_LEN_CMD-3
+
+#define FIRST_BYTE_EMPTY  0x80
+
+
+typedef union {
+  byte BYTE;
+  struct {
+    byte B0 :1;  
+    byte B1 :1;  
+    byte B2 :1;  
+    byte B3 :1;  
+    byte B4 :1;  
+    byte B5 :1;  
+    byte B6 :1;  
+    byte B7 :1;  
+  } BIT;
+} byte_def;
+  
 
 
 void comm_init(int (*putch_)(char), int (*getch_)(void), int(*kbhit_)(void));
