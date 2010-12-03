87e0c9856d741993e731e0938007df8e542a2d91
fantuz76@gmail.com <fantuz76@gmail.com@ba4d8bbf-c410-466b-920d-f5df8fa453a8>
2010-12-03 16:18:04 +0000

    Primi inizi

--- ChiUSB_Sources/protocol.c
+++ ChiUSB_Sources/protocol.c
@@ -1,18 +1,27 @@
 #include "usb_cdc.h" 
 #include "terminal/hcc_terminal.h"
 #include "protocol.h"
+#include <stdtypes.h>
 
 
 /*****************************************************************************
  * Module variables.
  *****************************************************************************/
 
-static hcc_u8 usbNumPktLen;
+static byte usbNumByteLen;
+static byte usbPayloadLen;
 
 static char usbrxar[MAX_LEN_CMD];
-static hcc_u8 usbrxar_ndx;
+//static char *usbrxar;
+static byte usbrxar_ndx;
 
-//static hcc_u8 n_cmd;
+
+byte_def _USB_flags1;
+#define USB_flags1    _USB_flags1.BYTE
+#define USBPktReady   _USB_flags1.BIT.B0
+//#define xxx    _USB_flags1.BIT.B1
+
+//static byte n_cmd;
 //static command_t * cmds[MAX_CMDS];
 
 
@@ -20,6 +29,13 @@ static int (*putch)(char);
 static int (*getch)(void);
 static int (*kbhit)(void);
 
+
+void ResetPkt_ndx(void){
+  usbrxar_ndx = 0;
+  usbPayloadLen = 0;
+  usbNumByteLen = 0;        
+  USBPktReady = FALSE;
+}
 
 
 /*****************************************************************************
@@ -41,7 +57,9 @@ static int (*kbhit)(void);
 void comm_init(int (*putch_)(char), int (*getch_)(void), int(*kbhit_)(void))
 {
   usbrxar[sizeof(usbrxar)-1]='\0';
-  usbrxar_ndx=0;
+  
+  
+  ResetPkt_ndx();
 
   //cmds[0]=(void *)&help_cmd;
   //n_cmd=1;
@@ -71,16 +89,73 @@ void comm_init(int (*putch_)(char), int (*getch_)(void), int(*kbhit_)(void))
 * Assumptions:
 *    --
 *****************************************************************************/
-hcc_u8 ParseFirstByte(hcc_u8 _byteToParse) 
+byte ParseFirstByte(byte _byteToParse) 
 {
-  if ((_byteToParse & 0xF0) == 0x80) {    
-    usbNumPktLen = (_byteToParse & 0x03);
+  if ((_byteToParse & 0xF0) == FIRST_BYTE_EMPTY) {    
+    usbNumByteLen = (_byteToParse & 0x03);
   } else {
     // Error - pkt unknown
     return(0);        
+  }  
+}
+
+
+
+
+/*****************************************************************************
+ * Name:
+ *    USB_PktSend
+ * In:
+ *    _toSend: 
+ * Out:
+ *    n/a
+ *
+ * Description:
+ *    Print the specified string.
+ * Assumptions:
+ *
+ *****************************************************************************/
+void USB_PktSend(byte *_toSend, byte _len)
+{
+  byte cnt=0, byteToSend, cks=0;
+  
+  byteToSend = FIRST_BYTE_EMPTY;  
+  if (_len==1) byteToSend |= 0x01;
+  
+  
+  // Primo Byte
+  while(byteToSend != (char)cdc_putch(byteToSend))
+    ;  
+  cks += byteToSend;
+
+  // Byte Length
+  if (_len==1){    
+    byteToSend = _len;
+    while(byteToSend != (char)cdc_putch(byteToSend))
+      ;
+    cks += byteToSend;
   }
   
+  
+  // PayLoad
+  while (cnt<_len){  
+    while(*_toSend != (char)cdc_putch(*_toSend))
+      ;
+    cks += (*_toSend);
+    _toSend++;
+    cnt++;
+  }
+  
+  
+  // CheckSum
+  while(cks != (char)cdc_putch(cks))
+    ;
+  
 }
+   
+ 
+ 
+   
                       
 /*****************************************************************************
 * Name:
@@ -100,6 +175,9 @@ hcc_u8 ParseFirstByte(hcc_u8 _byteToParse)
 void comm_process(void)
 {
 
+  byte USBtoSend[MAX_LEN_PAYLOAD];
+  byte i;
+  byte cks;
   // finchè c'è qualcosa nel buffer USB
   // Se ci sono più di 32 byte entro + volte qua
   
@@ -110,25 +188,105 @@ void comm_process(void)
     c=(char)(cdc_getch)();        
     usbrxar[usbrxar_ndx++]=c;
     
-    __asm("nop");
+    if (usbrxar_ndx >= MAX_LEN_CMD)   // Overflow
+      ResetPkt_ndx();
     
-    if (usbrxar_ndx==1) {
+   
+    
+    if (usbrxar_ndx-1==0) {    
       // primo byte rx
- /*     if (ParseFirstByte(usbrxar[usbrxar_ndx-1]) == 0) {
-        // Parse KO        
-        usbrxar_ndx = 0;
+      if ((usbrxar[usbrxar_ndx-1] & 0xF0) == FIRST_BYTE_EMPTY) {        
+        // C'è la Lunghezza?  
+        usbNumByteLen = (usbrxar[usbrxar_ndx-1] & 0x01);        
+                    
+      } else {
+        // Error - pkt unknown
+        ResetPkt_ndx();
+      }      
+
+    } else if ( (usbNumByteLen==1) &&  (usbrxar_ndx-1==1)) {
+      // Sono in lunghezza      
+        usbPayloadLen = usbrxar[1];         
+    
+    } else if ((usbrxar_ndx-1 >= usbNumByteLen+1)  && (usbrxar_ndx-1 < usbPayloadLen+usbNumByteLen+1)) {
+      // Sono in PayLoad
+            
+      
+    } else if (usbrxar_ndx-1 == usbPayloadLen+usbNumByteLen+1) {
+      // Sono in ChkSum
+      for (i=0,cks=0; i<usbrxar_ndx-1; cks += usbrxar[i++]);
+              
+      if (cks != usbrxar[i])
+      {
+        ResetPkt_ndx();
+      } else {
+        USBPktReady = TRUE;        
       }
       
- */     
-    } else if (usbNumPktLen) {
+    
+    } else {
+      // altro?
+      ResetPkt_ndx();
       
     }
     
     
-    if (usbrxar_ndx==MAX_LEN_CMD) {
-      usbrxar_ndx=0;
+    
+    
+    if (USBPktReady) {
+      
+      // Analizzare messaggio ricevuto
+      
+      if (usbPayloadLen==0) {      
+        USB_PktSend(USBtoSend,0); 
+      } else {
+        switch (usbrxar[usbPayloadLen+1]) {
+          case 0x33:
+            USBtoSend[0] = 0x30;
+            USBtoSend[1] = 0x31;
+            USBtoSend[2] = 0x32;
+            USBtoSend[3] = 0x33;
+            
+            USB_PktSend(USBtoSend,3);          
+          break;
+
+
+          case 0x41:
+            USBtoSend[0] = 0x40;
+            USBtoSend[1] = 0x40;
+            if (SW1_ACTIVE()) {
+              USBtoSend[0] |= 0x05;
+              
+            }
+
+            if (SW2_ACTIVE()) {
+              USBtoSend[1] |= 0x07;
+              
+            }
+          
+            
+            USBtoSend[2] = 0x42;
+            USBtoSend[3] = 0x43;
+            
+            USB_PktSend(USBtoSend,4);          
+          break;
+
+          default:
+            USBtoSend[0] = 0x55;
+            USB_PktSend(USBtoSend,1);
+          break;          
+        }
+      
+      }
+      
+
+      ResetPkt_ndx();      
+      
     }
-   
+    
+    
+    
+    
     
     // echo
     //while(c!=(char)(putch)(c))
