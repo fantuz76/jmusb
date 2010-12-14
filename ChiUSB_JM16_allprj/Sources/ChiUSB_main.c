45bf33ee064dad5909af23dc9d12aa30a9f5a435
fantuz76@gmail.com <fantuz76@gmail.com@ba4d8bbf-c410-466b-920d-f5df8fa453a8>
2010-12-14 17:01:20 +0000

    Ricomposto il progetto all'interno della cartella ChiUSB_JM16_prj Riordinati tutti i file Source dentro una cartella C'è ancora anche il vecchio in usb-peripheral/projects/CodeWarrior/hc9S08jm60/cdc-terminal-demo/cdc-demo.mcp

---
+++ ChiUSB_JM16_allprj/Sources/ChiUSB_main.c
@@ -0,0 +1,79 @@
+/****************************************************************************
+ *
+ *            Copyright (c) 2006-2007 by CMX Systems, Inc.
+ *
+ * This software is copyrighted by and is the sole property of
+ * CMX.  All rights, title, ownership, or other interests
+ * in the software remain the property of CMX.  This
+ * software may only be used in accordance with the corresponding
+ * license agreement.  Any unauthorized use, duplication, transmission,
+ * distribution, or disclosure of this software is expressly forbidden.
+ *
+ * This Copyright notice may not be removed or modified without prior
+ * written consent of CMX.
+ *
+ * CMX reserves the right to modify this software without notice.
+ *
+ * CMX Systems, Inc.
+ * 12276 San Jose Blvd. #511
+ * Jacksonville, FL 32223
+ * USA
+ *
+ * Tel:  (904) 880-1840
+ * Fax:  (904) 880-1632
+ * http: www.cmx.com
+ * email: cmx@cmx.com
+ *
+ ***************************************************************************/
+#include "usb.h"
+#include "target.h"
+#include "usb_cdc.h"
+#include "utils.h"
+#include "USB_man.h"
+
+
+/****************************************************************************
+ ************************** Macro definitions *******************************
+ ***************************************************************************/
+/* none */
+
+/****************************************************************************
+ ************************** Function predefinitions. ************************
+ ***************************************************************************/
+/* none */
+
+/****************************************************************************
+ ************************** Global variables ********************************
+ ***************************************************************************/
+/* none */
+
+/****************************************************************************
+ ************************** Module variables ********************************
+ ***************************************************************************/
+/* none */
+
+/****************************************************************************
+ ************************** Function definitions ****************************
+ ***************************************************************************/
+
+
+
+void main()
+{
+  hw_init();
+  
+  usb_cfg_init();
+
+  cdc_init();
+
+  comm_init();
+
+  /* This loop will gateway charactert from the UART to the USB and back. */
+  while(1)
+  {
+    comm_process();
+    cdc_process();
+  }
+ 
+}
+/****************************** END OF FILE **********************************/
